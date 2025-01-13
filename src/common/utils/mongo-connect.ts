e993c63784ee5181d75719483a07b5003f4095dc
Sujin Choi <sujin.2f@gmail.com>
2025-01-13 16:51:43 -0500

    mongo connection

--- src/common/utils/mongo-connect.ts
+++ src/common/utils/mongo-connect.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose'
 import { Error } from 'src/common/model/Error'
 
 export const mongoConnect = async (): Promise<typeof mongoose> => {
-    const host = process.env.MONGO || 'localhost:27017'
+    const host = process.env.MONGO
     const user = process.env.MONGO_USER
     const pass = process.env.MONGO_PASS
-    const uri = `mongodb://${user}:${pass}@${host}`
+    const userPass = user && pass ? `${user}:${pass}@` : ``
+    const uri = `mongodb://${userPass}${host}:27017`
     const dbName = process.env.MONGO_DATABASE
 
     console.log(`Mongo DB connection: ${uri} database ${dbName}`)
