bd81055da44a44bbc0ed40b020ae3c1fc96d9bf1
Sujin Choi <sujin.2f@gmail.com>
2026-03-20 15:14:33 -0400

    Focus bookmark

--- src/types/focus.ts
+++ src/types/focus.ts
@@ -1,3 +1,40 @@
+interface NavigationEntry {
+    // Docs: https://electronjs.org/docs/api/structures/navigation-entry
+
+    /**
+     * A base64 encoded data string containing Chromium page state including
+     * information like the current scroll position or form values. It is committed by
+     * Chromium before a navigation event and on a regular interval.
+     */
+    pageState?: string
+    title: string
+    url: string
+}
+
+// To send url & title via IPC
+export type T_Bookmark_Partial = {
+    url: string
+    title: string
+}
+
+// ⚓️ Anchor
+export type T_Anchor = T_Bookmark_Partial & {
+    uid: number
+    id: string
+}
+
+// 🔖 Bookmark
+export type T_Bookmark = T_Anchor &
+    NavigationEntry & {
+        uid?: number
+        shortcut?: string
+        parent?: string
+        dir?: boolean
+    }
+
+/**
+ * @deprecated
+ */
 export type T_Focus_Message = {
     _id: string
     title: string
